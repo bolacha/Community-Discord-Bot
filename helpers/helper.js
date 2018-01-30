bbe981c5926923aeabda6abd13ec179584a28808
Lucas Costa Oliveira <lucas@ulcas.com>
2018-01-30 01:02:09 -0200

    Added the module.require instead of the normal require, basically we saying that is the only `module` that will be using those libraries

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,5 +1,5 @@
-const Discord   = require('discord.js');
-const fs        = require('fs');
+const Discord   = module.require('discord.js');
+const fs        = module.require('fs');
 
 module.exports.setCommands = (client, folderOnRoot = 'commands') => {
 
