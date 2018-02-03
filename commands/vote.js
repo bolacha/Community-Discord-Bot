1e74a8777e6d61155ba54cf9b6ea7e3e58def5a4
Lucas Costa Oliveira <lucas@ulcas.com>
2018-02-03 17:58:21 -0200

    Added the Vote System , need some work.

---
+++ commands/vote.js
@@ -0,0 +1,28 @@
+const agree = "✅";
+const disagree = "❎";
+const time = 5000;
+
+module.exports.run = async (bot, message, args) => {
+
+    console.log(args);
+    console.log(message.content);
+
+    let msg = await message.channel.send(`Voting for : \n\n ${ args.join(" ") }\n\n by Author : ${message.author}`);
+
+    await msg.react(agree);
+    await msg.react(disagree);
+
+    const reactions = await msg.awaitReactions((reaction) => {
+
+        return reaction.emoji.name === agree || reaction.emoji.name === disagree;
+    }, { time });
+
+    const positive = ( !reactions.get(agree)    ? 0  : reactions.get(agree).count - 1 );
+    const negative = ( !reactions.get(disagree) ? 0  : reactions.get(disagree).count - 1 );
+
+    message.channel.send(`Vote results : \n\n ${agree} : ${ positive } \n\n ${disagree} : ${ negative }`);
+}
+
+module.exports.help = {
+    name: 'vote'
+}
